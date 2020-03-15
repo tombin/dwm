9a671b9b4ef3a856cfe8fcc25cc1f3a90e405956
tombin <tomb1n@yahoo.com>
2020-03-15 07:39:58 +0000

    boo

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 0;       /* gap pixel between windows */
+static const unsigned int gappx     = 25;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -52,7 +52,7 @@ static const Rule rules[] = {
 	 */
 	/* class                      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",                     NULL,       NULL,       0,            1,           -1 },
-	{ "firefox",                  NULL,       NULL,       1 << 8,       1,           -1 },
+	{ "firefox",                  NULL,       NULL,       1 << 8,       0,           -1 },
     { "install4j-burp-StartBurp", NULL,       NULL,       1 << 7,       1,           -1 },
 };
 
